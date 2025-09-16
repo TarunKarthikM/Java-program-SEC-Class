4fd15455823839725e4740b6cf48805b5a3669c0
Tarun karthik M <tarunkarthik.manivasagam@gmail.com>
2025-09-16 15:22:55 +0530

    Add files via upload

---
+++ ParanthesesOverride.java
@@ -0,0 +1,6 @@
+public class ParanthesesOverride{
+    public static void main(String[]args){
+        int result=(10+5)*2;
+        System.out.println("Result with parantheses:"+result);
+    }
+    }
